20d6e8ee8e7df529728b2dac11c45c01aa6acdda
F.T <123622255+tf155437@users.noreply.github.com>
2024-03-21 07:19:15 +0000

    Portfolio

--- Kino/abfragen.sql
+++ Kino/abfragen.sql
@@ -1,4 +1,36 @@
-SELECT * FROM Film;
-SELECT * FROM Regisseur;
+SELECT Regisseur.Vorname, Regisseur.Nachname, COUNT(Film.RegisseurID) AS Anzahl_der_Filme
+FROM Regisseur 
+LEFT JOIN Film ON Regisseur.RegisseurID = Film.RegisseurID GROUP BY Regisseur.Nachname
 
 
+SELECT*FROM Film;
+SELECT*FROM Regisseur;
+
+
+// 1
+SELECT DISTINCT Jahr FROM Film; 
+
+//2
+SELECT*FROM Film ORDER BY Länge DESC;
+
+//3
+SELECT Film.Titel, Regisseur.Vorname, Regisseur.Nachname 
+FROM Film
+Inner Join Regisseur ON Film.RegisseurID = Regisseur.RegisseurID;
+
+//4
+SELECT AVG(Länge) AS Durchschnittliche_Länge
+FROM Film; 
+
+//5 
+SELECT Regisseur.Nachname, Count(Film.RegisseurID) AS Anzahl_der_Filme
+FROM Regisseur
+Left Join Film ON Regisseur.RegisseurID = Film.RegisseurID
+Group by Regisseur.Nachname; 
+
+//6
+SELECT Jahr, Count(*) AS Anzahl_der_Filme
+FROM Film
+Group by Jahr
+Order by Anzahl_der_Filme DESC 
+Limit 1; 
--- Kino/insertKino.sql
+++ Kino/insertKino.sql
@@ -14,3 +14,22 @@ INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES
 ('Der Marsianer', 151, 2015, 4),     -- Ridley Scott
 ('Intime', 109, 2011, 5);            -- Michel Hazanavicius
  
+ INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Harry Potter 2', 161, 2002, 1);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Aquaman', 143, 2018, 2);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Apocalypse Now', 153, 1979, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Blade Runner', 117, 1982, 4);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Artist', 100, 2011, 5);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Harry Potter 3', 142, 2004, 1);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Godfather Part II', 200, 1974, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('The Godfather Part III', 162, 1990, 3);
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Alien', 117, 1979, 4); -- obwohl es ein Sci-Fi Horrorfilm ist, könnte er aufgrund seiner historischen Bedeutung und künstlerischen Qualität für ältere Jugendliche geeignet sein
+
+INSERT INTO Film (Titel, Länge, Jahr, RegisseurID) VALUES ('Gladiator', 155, 2000, 4);
