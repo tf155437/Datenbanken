5984a3b9db73b7a0f1b7992005c11c7c6e0f3000
F.T <123622255+tf155437@users.noreply.github.com>
2024-04-18 07:08:39 +0000

    Join 1/2

--- Bestellungen/Abfragen.sql
+++ Bestellungen/Abfragen.sql
@@ -1,2 +1,12 @@
 select* from Orders; 
 select * from Customers; 
+
+SELECT * 
+FROM Customers
+Inner Join Orders
+On Customers.CustomerID = Orders.CustomerID
+
+SELECT * 
+FROM Customers
+Left Join Orders
+On Customers.CustomerID = Orders.CustomerID
